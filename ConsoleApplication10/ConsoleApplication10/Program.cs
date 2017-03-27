2e93ac3eeeace208f33a9da8c8b926940c089920
pk3000000 <pk3000000@nate.com>
2017-03-27 12:06:33 +0900

    abc

---
+++ ConsoleApplication10/ConsoleApplication10/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 4, 6, 7, 8, 9, 10 };
+            int num = 7;
+
+            Console.WriteLine(findMid(ref arr, 0, arr.Length, num));
+        }
+
+
+      
+        /*
+        static void findMid(ref int [] arr, int first, int last, int num)
+        {
+            int mid = (first + last) / 2;
+
+            if (first > last)
+            {
+                Console.WriteLine("못 찾았습니다.");
+                return;
+            }
+            
+            if(arr[mid] > num)
+            {
+                findMid(ref arr, first, mid - 1, num);
+            }
+            else if(arr[mid] < num)
+            {
+                findMid(ref arr, mid + 1, last, num);
+            }
+            else if(arr[mid] == num)
+            {
+                Console.WriteLine("index = {0}", mid);
+                return;
+            }
+        }*/
+
+         
+        static int findMid(ref int[] arr, int first, int last, int num)
+        {
+            int mid = (first + last) / 2;
+
+            if (first > last)
+            {
+                Console.WriteLine("못 찾았습니다.");
+                return int.MinValue;
+            }
+            else
+            {
+                if (arr[mid] > num)
+                {
+                    mid = findMid(ref arr, first, mid - 1, num);
+                }
+                else if (arr[mid] < num)
+                {
+                    mid = findMid(ref arr, mid + 1, last, num);
+                }
+                else if (arr[mid] == num)
+                {
+                    return mid;
+                }
+            }
+            
+            return mid;
+        }
+        
+        static int numIndex(ref int[] arr, int num)
+        {
+            int first=0, last=arr.Length-1, mid = arr.Length / 2 - 1;
+
+            while(true)
+            {
+                if(arr[mid]==num)
+                {
+                    return mid;
+                }
+                else if(arr[mid] > num)
+                {
+                    last = mid - 1;
+                }
+                else if(arr[mid] < num)
+                {
+                    first = mid + 1;
+                }
+                
+                if(first >= last)
+                {
+                    break;
+                }
+
+                mid = (first + last) / 2;
+            }
+
+            Console.WriteLine("못 찾았습니다.");
+
+            return int.MinValue;
+        }
+    }
+}
