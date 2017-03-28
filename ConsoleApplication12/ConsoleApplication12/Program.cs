77e056d9f1422617077c3f3cd413d2e8420ad5d3
pk3000000 <pk3000000@nate.com>
2017-03-28 14:06:58 +0900

    rb

---
+++ ConsoleApplication12/ConsoleApplication12/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 5, 8, 11, 7, 6, 1, 4, 3,9 };
+            quickSort(ref arr, 0, arr.Length-1);
+
+            for(int i=0;i<arr.Length;i++)
+            {
+                Console.Write("{0} ", arr[i]);
+            }
+            Console.WriteLine();
+        }
+
+        static void quickSort(ref int[] arr,int start, int end)
+        {
+
+            if (start < end)
+            {
+                int left = start;
+                int right = end;
+
+                int pivot = (start + end) / 2-1;
+
+                while (left < right)
+                {
+                    while (arr[right] > arr[pivot])
+                    {
+                        right--;
+                    }
+                    while (left < right && arr[left] <= arr[pivot])
+                    {
+                        left++;
+                    }
+
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                }
+
+                int temp1 = arr[pivot];
+                arr[pivot] = arr[left];
+                arr[left] = temp1;
+               
+                quickSort(ref arr, start, left-1);
+                quickSort(ref arr, left + 1, end);
+            }
+        }
+    }
+}
---
+++ RedBlackTree/RedBlackTree/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBlackTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode parent;
+
+        public TreeNode(int val)
+        {
+            this.left = null;
+            this.right = null;
+            this.parent = null;
+            this.val = val;
+        }
+    }
+
+    public class RBTree
+    {
+        TreeNode root;
+        TreeNode nil;
+
+        public RBTree()
+        {
+            root = null;
+            nil = new TreeNode(int.MinValue);
+        }
+
+        public void leftRotate(ref TreeNode tempNode, ref TreeNode tNode)
+        {
+            TreeNode temp = tNode.right;
+            tNode.right = temp.left;
+
+            if(temp.left != nil)
+            {
+                temp.left.parent = tNode;
+            }
+
+            temp.parent = tNode.parent;
+
+            if(tNode.parent == )
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
